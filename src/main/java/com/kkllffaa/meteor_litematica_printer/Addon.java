acf094d559c3aa7edcd5f61923049f60e825de84
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-25 18:15:06 -0500

    Update Addon.java

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -1,21 +1,22 @@
 package com.kkllffaa.meteor_litematica_printer;
 
 import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
+import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Addon extends MeteorAddon {
     public static final Logger LOG = LoggerFactory.getLogger("Meteor Litematica Printer");
+    // This line fixes the "cannot find symbol CATEGORY" error
+    public static final Category CATEGORY = new Category("Printer", Items.ORANGE_CONCRETE.getDefaultStack());
 
     @Override
     public void onInitialize() {
         LOG.info("Initializing Meteor Litematica Printer with Spectator Plus");
 
-        // Keeps your Printer module working
         Modules.get().add(new Printer());
-        
-        // Adds your new Spectator module
         Modules.get().add(new SpectatorModule());
     }
 

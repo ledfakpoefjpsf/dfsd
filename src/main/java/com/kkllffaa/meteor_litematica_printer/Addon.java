5425f2225a1477613a315a2cdb41c0a56573e668
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-26 11:56:10 -0500

    Update Addon.java

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -1,15 +1,12 @@
 package com.kkllffaa.meteor_litematica_printer;
 
 import meteordevelopment.meteorclient.MeteorClient;
-import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 
 public class Addon {
     public static void init() {
-        // Get the module registry
-        Modules modules = MeteorClient.getInstance().modules;
-
-        // Register modules
+        // Register modules with Meteor Client
+        Modules modules = MeteorClient.modules;
         modules.add(new CreativeSurvivalModule());
         modules.add(new SpectatorModule());
     }

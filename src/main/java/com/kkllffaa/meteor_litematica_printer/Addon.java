d01633caded20ba658cd1a856bfe85282cf61322
ledfakpoefjpsf <lesterjosh124@gmail.com>
2026-03-26 10:55:38 -0500

    Refactor Addon class to register multiple modules

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Addon.java
@@ -6,28 +6,35 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Import your modules
+import com.kkllffaa.meteor_litematica_printer.SpectatorModule;
+import com.kkllffaa.meteor_litematica_printer.BowSlayer;
+
 public class Addon extends MeteorAddon {
-    public static final Logger LOG = LoggerFactory.getLogger("Spectator Plus");
-    
-    // Define the category here
-    public static final Category CATEGORY = new Category("Custom");
+public static final Logger LOG = LoggerFactory.getLogger("Spectator Plus");
 
-    @Override
-    public void onInitialize() {
-        LOG.info("Initializing Spectator Plus Addon...");
-        
-        // Only add the module here
-        Modules.get().add(new SpectatorModule());
-    }
+```
+// Category (tab in Meteor)
+public static final Category CATEGORY = new Category("Custom");
 
-    // FIX: This is the specific "Callback" the error message is asking for
-    @Override
-    public void onRegisterCategories() {
-        Modules.registerCategory(CATEGORY);
-    }
+@Override
+public void onInitialize() {
+    LOG.info("Initializing Spectator Plus Addon...");
 
-    @Override
-    public String getPackage() {
-        return "com.kkllffaa.meteor_litematica_printer";
-    }
+    // Register modules here
+    Modules.get().add(new SpectatorModule());
+    Modules.get().add(new BowSlayer());
+}
+
+@Override
+public void onRegisterCategories() {
+    Modules.registerCategory(CATEGORY);
+}
+
+@Override
+public String getPackage() {
+    return "com.kkllffaa.meteor_litematica_printer";
+}
+```
+
 }
